c1249d975bc3f0d4b7e72f30ea0690b68e110984
Blaze <ryankorteway@me.com>
2016-11-02 10:06:14 -0400

    TO do added to main and main tested successful

--- src/com/company/com/company/Main.java
+++ src/com/company/com/company/Main.java
@@ -12,10 +12,11 @@ public class Main {
         //to carry our our client based methods, like asking the server who has a file, and then getting the
         //file from that user once their ip address and port numbers are given.
         ClientToPeer ourClientToPeer = new ClientToPeer();
+        ClientToServer ourClientToServer = new ClientToServer();
         //ourClientToPeer.connect("12", 12); //unnecessary line to make sure that our client methods actually are reachable through
         //the client object.
 
-        NapsterView ourView = new NapsterView(ourClientToPeer);
+        NapsterView ourView = new NapsterView(ourClientToPeer); //TODO pass in the client to server stuff too.
     }
 
 }
